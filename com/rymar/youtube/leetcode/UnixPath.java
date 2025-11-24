9dc2e27cd2b86b06a363988d93e4923c80c52654
OleksandrRym <lostmannight@gmail.com>
2025-11-24 23:23:48 +0200

    add task Unix Path

---
+++ com/rymar/youtube/leetcode/UnixPath.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.Stack;
+
+class UnixPath {
+  public static void main(String[] args) {
+    String path = "////../";
+    simplifyPath(path);
+  }
+
+  public static String simplifyPath(String path) {
+    Stack stack = new Stack<>();
+    var folders = path.split("/");
+    for (String folder : folders) {
+      if (folder.equals(".") || folder.isEmpty()) continue;
+      if (!folder.equals("..")) stack.push(folder);
+      else if (!stack.isEmpty()) stack.pop();
+    }
+    return "/" + String.join("/", stack);
+  }
+}
