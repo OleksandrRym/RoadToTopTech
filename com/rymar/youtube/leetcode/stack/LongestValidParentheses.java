4e9ab1824ae10956b4026b662dc4daa6548dc40f
OleksandrRym <lostmannight@gmail.com>
2025-11-29 21:14:44 +0200

    update task LongestValidParentheses.java

--- com/rymar/youtube/leetcode/stack/LongestValidParentheses.java
+++ com/rymar/youtube/leetcode/stack/LongestValidParentheses.java
@@ -4,23 +4,23 @@ import java.util.Stack;
 
 public class LongestValidParentheses {
   public static void main(String[] args) {
-    longestValidParentheses("()");
+    longestValidParentheses("())))))(()()()");
   }
 
   public static int longestValidParentheses(String s) {
     var chars = s.toCharArray();
-    Stack stack = new Stack<Character>();
+    var stack = new Stack<Integer>();
     stack.push(-1);
     int max_len = 0;
     for (int i = 0; i < chars.length; i++) {
-      if (s.charAt(i) == '(') {
+      if (chars[i] == '(') {
         stack.push(i);
       } else {
         stack.pop();
         if (stack.isEmpty()) {
           stack.push(i);
         } else {
-          max_len = Math.max(max_len, i - (int) stack.peek());
+          max_len = Math.max(max_len, i - stack.peek());
         }
       }
     }
