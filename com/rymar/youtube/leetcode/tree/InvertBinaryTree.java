90ddaf976d603e70b3f1265fc7a2bac3dd04f464
OleksandrRym <lostmannight@gmail.com>
2025-12-01 09:20:04 +0200

    add task InvertBinaryTree.java

---
+++ com/rymar/youtube/leetcode/tree/InvertBinaryTree.java
@@ -0,0 +1,15 @@
+package leetcode.tree;
+
+import leetcode.tree.utill.TreeNode;
+
+public class InvertBinaryTree {
+
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null) return null;
+    TreeNode right = invertTree(root.right);
+    TreeNode left = invertTree(root.left);
+    root.left = right;
+    root.right = left;
+    return root;
+  }
+}
