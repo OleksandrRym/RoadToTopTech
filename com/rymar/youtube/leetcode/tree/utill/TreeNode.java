6cff84f06b16c1b420f54caacb573f8bc407d033
OleksandrRym <lostmannight@gmail.com>
2025-12-01 09:20:09 +0200

    add task InvertBinaryTree.java

---
+++ com/rymar/youtube/leetcode/tree/utill/TreeNode.java
@@ -0,0 +1,29 @@
+package leetcode.tree.utill;
+
+public class TreeNode {
+
+  public int val;
+  public TreeNode left;
+  public TreeNode right;
+
+  public TreeNode() {}
+
+  public TreeNode(int val) {
+    this.val = val;
+  }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
